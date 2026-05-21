efb1018809184db85f48117780c8c4f4c6c3e1c0
Nick Butler <nick@codeindulgence.com>
2026-05-21 19:30:08 +1000

    Nvim: Show the folder path not filename in lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -8,9 +8,15 @@ require('lualine').setup({
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', { 'diff', symbols = { added = ' ', modified = ' ', removed = ' ' } }, 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_c = {
+      { function()
+          local dir = vim.fn.expand('%:.:h')
+          if dir == '' then return '' end
+          return dir
+        end },
+    },
     lualine_x = { 'filetype' },
     lualine_y = { 'progress' },
-    lualine_z = { 'location' },
+    lualine_z = {},
   },
 })
