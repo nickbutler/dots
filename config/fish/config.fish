586406e50249586c48ad07cd6240d211ae991e3e
Nick Butler <nick@codeindulgence.com>
2026-05-22 18:59:56 +1000

    Dots: Make tmux optional, guard auto-start behind type check

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,10 @@
 if status --is-interactive
   source (starship init fish --print-full-init | psub)
-  if not set -q TMUX
+  if type -q tmux; and not set -q TMUX
     if not set -q SSH_CLIENT
       tmux -f ~/.config/tmux/tmux.conf new-session -A -s main -n Home
     end
-  else
+  else if set -q TMUX
     set -l window_name (tmux display -p '#{window_name}')
 
     # Bindings
