6e5c550391dc85c35d79a6646afdaa03096591b7
Nick Butler <nick@codeindulgence.com>
2026-05-22 19:14:37 +1000

    Fish: Guard starship init, fix set_color flags for fish 4.0 compatibility

--- config/fish/conf.d/colors.fish
+++ config/fish/conf.d/colors.fish
@@ -7,7 +7,7 @@ set fish_color_end b294bb
 set fish_color_error cc6666
 set fish_color_escape cyan
 set fish_color_history_current cyan
-set fish_color_host "-0 cyan"
+set fish_color_host cyan
 set fish_color_match cyan
 set fish_color_normal normal
 set fish_color_operator cyan
@@ -16,7 +16,7 @@ set fish_color_quote b5bd68
 set fish_color_redirection 8abeb7
 set fish_color_search_match --background=ccc
 set fish_color_status red
-set fish_color_user "-o green"
+set fish_color_user --bold green
 set fish_color_valid_path --underline
 
 set fish_pager_color_completion normal
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,7 @@
 if status --is-interactive
-  source (starship init fish --print-full-init | psub)
+  if type -q starship
+    source (starship init fish --print-full-init | psub)
+  end
   if type -q tmux; and not set -q TMUX
     if not set -q SSH_CLIENT
       tmux -f ~/.config/tmux/tmux.conf new-session -A -s main -n Home
