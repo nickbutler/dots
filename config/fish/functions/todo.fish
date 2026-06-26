dfaf7f55c42bc1f6a8258d10b46da89d6588f4d2
Nick Butler <nick@butler.email>
2026-06-26 16:53:25 +1000

    Fish: Update todo func for *.todo.md

--- config/fish/functions/todo.fish
+++ config/fish/functions/todo.fish
@@ -2,12 +2,12 @@ function todo
   set -q TODO_HOME; or set TODO_HOME ~/Documents/notes/todo
   set -q TODO_RTP; or set TODO_RTP ~/code/projects/todo.nvim
 
-  set TODO_FILE $TODO_HOME/main.todo
+  set TODO_FILE $TODO_HOME/main.todo.md
 
   if set -q argv[1]
     set TODO_FILE $argv[1]
 
-    if not string match -q '*/*' -- $TODO_FILE; and not string match -q '*.todo' -- $TODO_FILE
+    if not string match -q '*/*' -- $TODO_FILE; and not string match -q '*.todo.md' -- $TODO_FILE
       set TODO_FILE $TODO_HOME/$TODO_FILE.todo
     end
   end
